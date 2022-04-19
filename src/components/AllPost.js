04d28b09e735b9ddbd0557819e8db4726b8e37e8
Avani <avanivijwala31@gmail.com>
2022-04-19 17:34:05 +0530

    change file name

--- src/components/AllPost.js
+++ src/components/AllPost.js
@@ -1,12 +1,11 @@
 import { connect } from "react-redux";
 import Post from "./Post";
-import EditComponent from "./EditComponent";
+import EditComponent from "./EditPost";
 const AllPost = (props) => {
   return (
 
     <div className="p-2 my-2 border mx-auto">
       {console.log("post array are ::", props.posts)}
-      <h6>authentication branch</h6>
       <h4 className="text-primary text-center">All Posts</h4>
       {props.posts.map((post) => (
 
